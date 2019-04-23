e3202fdce6fb8f0e1dd4c3ba08004d3d3d741659
JeppeBarsoe <49067821+JeppeBarsoe@users.noreply.github.com>
2019-04-23 16:09:09 +0200

    FoxIntro
    Kode for lyd i tøjbutik

---
+++ Kodning 1/foxshopjava.js
@@ -0,0 +1,10 @@
+let fox = new Audio ();
+fox.src = "Lydfiler/FoxIntro.mp3";
+
+
+
+
+
+ setTimeout(function(){
+            window.location.href = 'http://lifeoflaurids.dk/projekt5/vende-spil3/index.html';
+         }, 11000);
