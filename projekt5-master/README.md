57fffd0e884358f8ae38b686312fa81a9f85cfbf
RasmusUCL <47382081+RasmusUCL@users.noreply.github.com>
2019-04-23 12:22:23 +0200

    Hvad er dit navn?
    Solen spørger efter dit navn, når siden loader.

---
+++ projekt5-master/README.md
@@ -0,0 +1,3 @@
+# Projekt 5
+Abtion's Storm app
+
---
+++ projekt5-master/StormAppJava.js
@@ -0,0 +1,13 @@
+let CloudPee = new Audio ();
+CloudPee.src = "Sound/Cloud_pee.mp3";
+
+let SunHappy = new Audio ();
+SunHappy.src = "Sound/Sun_Happy.mp3";
+
+window.onload=function(){
+    document.getElementById("No").play();
+}
+
+function ani(){
+    document.getElementById('img').className ='classname';
+}
